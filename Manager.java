4cd81e094db33c00021155b8c1e4e501dd3e58a7
apple6487 <80956829+apple6487@users.noreply.github.com>
2021-05-28 11:41:02 +0900

    EnterMovie user java 부분
    src.user

---
+++ Manager.java
@@ -0,0 +1,22 @@
+package user;
+
+public class Manager {
+	private String managerID;
+	private String managerPassword;
+	
+	public String getManagerID() {
+		return managerID;
+	}
+	public void setManagerID(String managerID) {
+		this.managerID = managerID;
+	}
+	public String getManagerPassword() {
+		return managerPassword;
+	}
+	public void setManagerPassword(String managerPassword) {
+		this.managerPassword = managerPassword;
+	}
+	
+	
+	
+}
